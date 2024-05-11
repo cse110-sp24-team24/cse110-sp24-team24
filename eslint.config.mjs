057c19cf3d15c31a74250cc818fdd4060911e24a
HarshGurnani <gurnani.harsh@gmail.com>
2024-05-11 16:00:36 -0700

    modified eslint configuration file to allow Jest and puppeteer commands

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        page: "readonly",
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
