de3c49748482f75f99d7daaff496149476320e6e
Azathotha <93633837+Azathotha@users.noreply.github.com>
2024-05-17 16:10:20 -0700

    add js and json file

---
+++ API.js
@@ -0,0 +1,13 @@
+function saveNote(note) {
+  localStorage.setItem("notes", JSON.stringify(note));
+}
+
+let newNote = {
+  id: "2",
+  title: "Meeting Agenda",
+  date: new Date(),
+  tags: ["meeting", "agenda"],
+  content: "Discuss quarterly goals and progress.",
+};
+
+saveNote(newNote);
