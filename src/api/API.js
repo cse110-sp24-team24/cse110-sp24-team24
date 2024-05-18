877cebba08da96a3742895151e05eb687c748137
Azathotha <93633837+Azathotha@users.noreply.github.com>
2024-05-17 18:32:29 -0700

    add save button

--- src/api/API.js
+++ src/api/API.js
@@ -1,7 +1,3 @@
-function saveNote(note) {
-  localStorage.setItem("notes", JSON.stringify(note));
-}
-
 let newNote = {
   id: "2",
   title: "Meeting Agenda",
@@ -10,24 +6,28 @@ let newNote = {
   content: "Discuss quarterly goals and progress.",
 };
 
-saveNote(newNote);
+//Save button
+const saveNoteButton = document.getElementById("SaveButton");
+saveNoteButton.addEventListener("click", () => saveNote(newNote));
 
 //Delete button
 let deleteButton = document.getElementById("DeleteButton");
-deleteButton.addEventListener("click", function() {
+deleteButton.addEventListener("click", function () {
   // Call the DeleteNote function with the id of the note you want to delete
   DeleteNote(newNote.id);
- }
-);
+});
+
+function saveNote(note) {
+  localStorage.setItem("notes", JSON.stringify(note));
+}
 
 function DeleteNote(noteId) {
-   // Grab the existing notes from local storage
-   let notes = JSON.parse(localStorage.getItem("notes"));
+  // Grab the existing notes from local storage
+  let notes = JSON.parse(localStorage.getItem("notes"));
 
-   // Filter out the note with the given id
-  notes = notes.filter(note => note.id !== noteId);
- 
-   // Save the remaining notes back to local storage
-   localStorage.setItem("notes", JSON.stringify(notes));
+  // Filter out the note with the given id
+  notes = notes.filter((note) => note.id !== noteId);
 
+  // Save the remaining notes back to local storage
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
