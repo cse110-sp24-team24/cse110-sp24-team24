483f68c534b93599a6b9e1f50fd1839222fd2f0c
David Choi <y.choi.david@gmail.com>
2024-05-23 15:26:18 -0700

    fileStorage module outline kinda ...

--- src/scripts/fileStorage.js
+++ src/scripts/fileStorage.js
@@ -1,4 +1,19 @@
-//import {fs} from "fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import path from "path";
+import fs from "fs/promises";
 
-const directory  = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");  
-console.log(directory);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dataPath = path.join(__dirname, "../data/data.json"); // HACK there should be a consistent way to start from the top level directory and not start from __dirname
+
+//take notes and save it to data.json
+function saveNotes() {
+    fs.writeFile(path.join());
+    console.log(dataDirectory);
+}
+
+//return an array from data.json
+// function readNotes() {
+//     console.log("test");
+// }
