9a7e0bcdecbba26938167a27a9f2d9e309526277
David Choi <y.choi.david@gmail.com>
2024-05-23 17:33:33 -0700

    updated fs

--- src/scripts/fileStorage.js
+++ src/scripts/fileStorage.js
@@ -1,19 +1,25 @@
 import { fileURLToPath } from "node:url";
 import { dirname } from "node:path";
 import path from "path";
-import fs from "fs/promises";
+import fs from "fs/promises"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const dataPath = path.join(__dirname, "../data/data.json"); // HACK there should be a consistent way to start from the top level directory and not start from __dirname
 
-//take notes and save it to data.json
-function saveNotes() {
-    fs.writeFile(path.join());
-    console.log(dataDirectory);
+//TODO create comments
+//TODO create tests
+async function saveNotes(notes){
+    const jsonData = JSON.stringify(notes);
+    try{
+        await fs.writeFile(dataPath,jsonData);
+    }
+    catch(error){
+        console.error(error);
+    }
 }
 
-//return an array from data.json
-// function readNotes() {
-//     console.log("test");
-// }
+async function readNotes(){
+}
+
+export default {saveNotes, readNotes}; //access via import fileStorage from ./fileStorage.js
