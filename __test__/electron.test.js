12923cd8c0a0e52d773c0176a2645d421e6a6472
geenalimfat <glimfat@ucsd.edu>
2024-06-06 19:24:36 -0700

    Update electron.test.js

--- __test__/electron.test.js
+++ __test__/electron.test.js
@@ -2,3 +2,21 @@ describe("Testing Window Functionality", () => {
   //launch the app check its on darwin
   //if you close a window with pupeteer confirm the app is still running and make sure you can stil open it
 });
+const {app, BrowserWindow} = require("electron");
+const pie = require("puppeteer-in-electron")
+const puppeteer = require("puppeteer-core");
+
+const main = async () => {
+  await pie.initialize(app);
+  const browser = await pie.connect(app, puppeteer);
+ 
+  const window = new BrowserWindow();
+  const url = "https://example.com/";
+  await window.loadURL(url);
+ 
+  const page = await pie.getPage(browser, window);
+  console.log(page.url());
+  window.destroy();
+};
+
+main();
