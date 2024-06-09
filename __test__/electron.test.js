fbb7bf35cd480a98ce801c725bbc3be3c285c141
kieraliz <kenavarro@ucsd.edu>
2024-06-08 23:01:37 -0700

    Update electron.test.js
    Last test isn't working "save note", fails to click "save"

--- __test__/electron.test.js
+++ __test__/electron.test.js
@@ -10,21 +10,20 @@ describe("Testing Window Functionality", () => {
     browser = await puppeteer.launch({
       executablePath: require("electron"),
       args: [path.resolve("./src/index.html")],
+      slowMo: 25, // 25 milliseconds delay between operations
     });
 
     // Create a new page
-    const pages = await browser.pages(); // Get all open pages
-    page = pages[0]; // Use the first page (Electron window)
+    const pages = await browser.pages();
+    page = pages[0];
   });
 
   afterAll(async () => {
-    // Close the browser instance
     await browser.close();
   });
 
-  test("Open the main window", async () => {
-    // Example: Check if the page title matches
-    await page.waitForSelector("title"); // Wait for the title element to be available
+  test("Open the main window and check title", async () => {
+    await page.waitForSelector("title");
     const pageTitle = await page.title();
     expect(pageTitle).toBe("Dev Journal");
   });
@@ -37,42 +36,18 @@ describe("Testing Window Functionality", () => {
     // Navigate back to the app URL
     await page.goto("file://" + path.resolve("./src/index.html"));
 
-    // Wait for the title element to be available
     await page.waitForSelector("title");
-    // Get the title of the page
     const newPageTitle = await page.title();
-
-    // Expect the title to match the expected title
     expect(newPageTitle).toBe("Dev Journal");
   });
 
-  test("Click Add Note and open the note editor window", async () => {
-    // Click the 'Add Note' button
+  test("Cancel note and close editor window", async () => {
     await page.click("#addNoteButton");
-
-    // Wait for the note editor to be displayed
     await page.waitForSelector("#noteEditor:not(.hidden)");
 
-    // Check if the note editor is displayed
-    const noteEditor = await page.$("#noteEditor");
-    const isNoteEditorVisible = await noteEditor.evaluate(
-      (el) => !el.classList.contains("hidden")
-    );
-
-    // Expect the note editor to be visible
-    expect(isNoteEditorVisible).toBe(true);
-  });
-
-  test("Cancel note", async () => {
-    await page.waitForSelector("#noteTitle");
-    await page.type("#noteTitle", "First test");
-
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-
-    // Click the 'Cancel' button
+    await page.waitForSelector("#cancelButton", { visible: true });
     await page.click("#cancelButton");
 
-    // Wait for the note editor to be hidden
     await page.waitForSelector("#noteEditor.hidden");
 
     // Check if the note editor is hidden
@@ -80,47 +55,20 @@ describe("Testing Window Functionality", () => {
       const noteEditor = document.querySelector("#noteEditor");
       return noteEditor.classList.contains("hidden");
     });
-
-    // Expect the note editor to be hidden
     expect(isNoteEditorHidden).toBe(true);
 
-    // Wait for the home page to return
-    await page.waitForSelector("title"); // Wait for the title element to be available
-    const pageTitle = await page.title();
-    expect(pageTitle).toBe("Dev Journal");
+    // Check if the save button is not clicked
+    const isSaveButtonClicked = await page.evaluate(() => {
+      const saveButton = document.querySelector("#saveNoteButton");
+      return saveButton.classList.contains("clicked");
+    });
+    expect(isSaveButtonClicked).toBe(false);
+  }, 20000);
 
-    await page.reload();
-  });
-
-  test("Fill note with details, save, and close", async () => {
-    // Click the 'Add Note' button
+  test("Add new note", async () => {
     await page.click("#addNoteButton");
-
-    // Wait for the note editor to be displayed
     await page.waitForSelector("#noteEditor:not(.hidden)");
 
-    // Wait for the input fields to appear
-    await page.waitForSelector("#noteTitle");
-    await page.waitForSelector("#noteTags");
-    await page.waitForSelector("#noteContent");
-    await page.waitForSelector("#noteDate");
-
-    // Fill in the note details
-    await page.type("#noteTitle", "Write unit tests");
-    await page.type("#noteTags", "TEST");
-    await page.type(
-      "#noteContent",
-      "Write unit tests using puppeteer and chat"
-    );
-  });
-
-  test("Observe for 30 seconds", async () => {
-    // Wait for 30 seconds
-    await new Promise((resolve) => setTimeout(resolve, 30000));
-  });
-
-  /*
-  test("Fill note with details, save, and close", async () => {
     // Wait for the input fields to appear
     await page.waitForSelector("#noteTitle");
     await page.waitForSelector("#noteTags");
@@ -160,114 +108,17 @@ describe("Testing Window Functionality", () => {
     expect(noteDateValue).toBe(tomorrowISOString);
     expect(noteTagsValue).toBe("TEST");
     expect(noteContentValue).toBe("Write unit tests using puppeteer and chat");
+  }, 150000);
 
-    // Click on the save button to save the note
-
-    await page.waitForSelector("#saveNoteButton");
-    await page.click("#saveNoteButton");
-
-    // Check if the note editor is displayed
-    const noteEditor = await page.$("#noteEditor");
-    const isNoteEditorVisible = await noteEditor.evaluate(
-      (el) => !el.classList.contains("hidden")
-    );
-
-    // Expect the note editor to be visible
-    expect(isNoteEditorVisible).toBe(false);
-  });
-  */
-
-  /**
-  test("Open another note", async () => {
-    // Click the 'Add Note' button
-    await page.click("#addNoteButton");
-
+  test("Save note", async () => {
     // Wait for the note editor to be displayed
     await page.waitForSelector("#noteEditor:not(.hidden)");
 
-    // Check if the note editor is displayed
-    const noteEditor = await page.$("#noteEditor");
-    const isNoteEditorVisible = await noteEditor.evaluate(
-      (el) => !el.classList.contains("hidden")
-    );
+    // Wait for the cancel button to be visible and clickable
+    await page.waitForSelector("#saveNoteButton", { visible: true });
 
-    // Expect the note editor to be visible
-    expect(isNoteEditorVisible).toBe(true);
-
-    await page.waitForSelector("#noteTitle");
-    await page.type("#noteTitle", "Cancel note");
-  });
-  */
-
-  /**
-  test("Back to home page after saving the note", async () => {
-    // Click on save button to save the note
+    // Click the cancel button
     await page.click("#saveNoteButton");
-
-    // Wait for the note editor to be hidden
-    await page.waitForSelector("#noteEditor.hidden");
-
-    // Check if the note editor is hidden
-    const noteEditorHidden = await page.$eval("#noteEditor", (editor) =>
-      editor.classList.contains("hidden")
-    );
-
-    // Expect the note editor to be hidden
-    expect(noteEditorHidden).toBe(true);
-
-    // Assert that the editor window is closed
-    const isEditorClosed = await page.$eval(
-      "#editorWindow",
-      (editor) => editor.style.display === "none"
-    );
-    expect(isEditorClosed).toBe(true);
-    */
-
-  /**
-    // Wait for the editor window to close
-    await page.waitForSelector("#editorWindow", { hidden: true });
-
-    // Assert that the editor window is closed
-    const isEditorClosed = await page.$eval(
-      "#editorWindow",
-      (editor) => editor.style.display === "none"
-    );
-    expect(isEditorClosed).toBe(true);
-    */
-
-  // Assert that we are in the home page again
-  // const currentURL = page.url();
-  // expect(currentURL).toMatch(/\/home$/);
-
-  /**
-    // Check if the note editor is displayed
-    const noteEditor = await page.$("#noteEditor");
-    const isNoteEditorVisible = await noteEditor.evaluate(
-      (el) => !el.classList.contains("hidden")
-    );
-
-    // Expect the note editor to be not visible
-    expect(isNoteEditorVisible).toBe(false);
-
-    // Verify that we're back to the home page by checking the page title
-    await page.waitForSelector("title"); // Wait for the title element to be available
-    const pageTitle = await page.title();
-    expect(pageTitle).toBe("Dev Journal");
-  }, 10000); // Set timeout to 10 seconds (10000 milliseconds) to ensure enough time for the page to load
-  */
-
-  /**
-  test("Cancel note", async () => {
-    // Click the 'Add Note' button
-    await page.click("#addNoteButton");
-
-    await page.type("#noteTitle", "Cancel this note");
-
-    // Wait for 2 seconds
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    // Click the 'Cancel' button
-    await page.click("#cancelButton");
 
     // Wait for the note editor to be hidden
     await page.waitForSelector("#noteEditor.hidden");
@@ -281,10 +132,13 @@ describe("Testing Window Functionality", () => {
     // Expect the note editor to be hidden
     expect(isNoteEditorHidden).toBe(true);
 
-    // back to main page
-    await page.waitForSelector("title"); // Wait for the title element to be available
-    const pageTitle = await page.title();
-    expect(pageTitle).toBe("Dev Journal");
-  }, 3000); // Set timeout to 3 seconds (3000 milliseconds) to account for the waiting time and the actual action
-  */
+    // Check if the save button is not clicked
+    const isSaveButtonClicked = await page.evaluate(() => {
+      const saveButton = document.querySelector("#saveNoteButton");
+      return saveButton.classList.contains("clicked");
+    });
+
+    // Expect the save button not to be clicked
+    expect(isSaveButtonClicked).toBe(true);
+  }, 30000);
 });
